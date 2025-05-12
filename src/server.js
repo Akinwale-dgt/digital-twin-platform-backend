bc36c5316711433b415bb04e73f53dd454b5e7be
Akinwale-dgt <okunolaakinwale@gmail.com>
2025-05-12 07:55:25 +0100

    Add cognitive load and discomfort analysis functionality

--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ import logger from './utils/customLogger.js'
 
 /* Express application */
 import app from './app.js'
+import {
+  analyze,
+  balance,
+  exertion,
+  overall_cognitive_load,
+  overall_discomfort,
+} from './service/analysis.js'
 
 /* Listen for uncaught exception event */
 process.on('uncaughtException', (err) => {
@@ -33,6 +40,78 @@ const server = app.listen(port, () =>
   logger.info(`Listening on port ${port} in ${process.env.NODE_ENV} mode.`),
 )
 
+try {
+  const inputData = {
+    cognitive_load: {
+      mental_demand: 10,
+      physical_demand: 5,
+      temporal_demand: 12,
+      performance: 8,
+      effort: 6,
+      frustration: 9,
+    },
+    discomfort: {
+      hand_wrist: 4,
+      chest: 3,
+      upper_arm: 1,
+      shoulder: 5,
+      lower_back: 6,
+      thigh: 0,
+      neck: 2,
+      lower_leg_foot: 1,
+    },
+    exertion: 14,
+    balance: 3,
+  }
+
+  analyze(inputData)
+  .then((result) => {
+    logger.info(`[Analysis Result]: ${JSON.stringify(result)}`)
+
+    // Ensure required fields exist
+    if (!result.cognitive_load || !result.discomfort || !result.exertion || !result.balance) {
+      throw new Error('Missing expected fields in model response')
+    }
+
+    // const sanitizedResult = {
+    //   cognitive_load: {
+    //     ...result.cognitive_load,
+    //     mental_demand: result.cognitive_load?.mental_demand ?? 0,
+    //     physical_demand: result.cognitive_load?.physical_demand ?? 0,
+    //     temporal_demand: result.cognitive_load?.temporal_demand ?? 0,
+    //     performance: result.cognitive_load?.performance ?? 0,
+    //     effort: result.cognitive_load?.effort ?? 0,
+    //     frustration: result.cognitive_load?.frustration ?? 0,
+    //   },
+    //   discomfort: {
+    //     ...result.discomfort,
+    //     hand_wrist: result.discomfort?.hand_wrist ?? 0,
+    //     chest: result.discomfort?.chest ?? 0,
+    //     upper_arm: result.discomfort?.upper_arm ?? 0,
+    //     shoulder: result.discomfort?.shoulder ?? 0,
+    //     lower_back: result.discomfort?.lower_back ?? 0,
+    //     thigh: result.discomfort?.thigh ?? 0,
+    //     neck: result.discomfort?.neck ?? 0,
+    //     lower_leg_foot: result.discomfort?.lower_leg_foot ?? 0,
+    //   },
+    //   exertion: result.exertion ?? 0,
+    //   balance: result.balance ?? 0,
+    // }
+
+
+    // logger.info(`[Analysis Result]: ${JSON.stringify(sanitizedResult)}`)
+  })
+  .catch((error) => {
+    logger.error(`[Analysis Error]: ${error.message}`)
+  })
+  .finally(() => {
+    logger.info('Analysis completed.')
+  })
+
+} catch (error) {
+  logger.error(`[Error]: ${error.message}`)
+  // process.exit(1)
+}
 /* Listen for unhandled promise rejection event */
 process.on('unhandledRejection', (err) => {
   logger.error(`[UnhandledRejection]: ${err}`)
--- src/service/analysis.js
+++ src/service/analysis.js
@@ -1,33 +1,72 @@
-import { OpenAI } from '@langchain/openai'
+import { ChatOpenAI } from '@langchain/openai'
 import dotenv from 'dotenv'
 import { RunnableSequence } from '@langchain/core/runnables'
 import { StructuredOutputParser } from '@langchain/core/output_parsers'
 import { PromptTemplate } from '@langchain/core/prompts'
+import { z } from 'zod'
 
 /* Load environment variables into Node.js process */
 dotenv.config()
 
 // const model = new ChatOpenAI({ model: 'gpt-4' })
-const model = new OpenAI({
+const model = new ChatOpenAI({
   model: process.env.OPENAI_MODEL,
-  temperature: 0,
+  temperature: 0.7,
   openAIApiKey: process.env.OPENAI_API_KEY,
 })
 
-export const analyze = async (text) => {
+export const analyze = async (inputData) => {
   const parser = StructuredOutputParser.fromZodSchema(
     z.object({
-      cognitive_load: overall_cognitive_load,
-      discomfort: overall_discomfort,
-      exertion,
-      balance,
+      cognitive_load: z.object({
+        raw_scores: overall_cognitive_load,
+        categories: z.object({
+          mental_demand: z.enum(['Low', 'Medium', 'High']),
+          physical_demand: z.enum(['Low', 'Medium', 'High']),
+          temporal_demand: z.enum(['Low', 'Medium', 'High']),
+          performance: z.enum(['Low', 'Medium', 'High']),
+          effort: z.enum(['Low', 'Medium', 'High']),
+          frustration: z.enum(['Low', 'Medium', 'High']),
+        }),
+        overall_score: z.number().min(0).max(120),
+        overall_category: z.enum(['Low', 'Medium', 'High']),
+      }),
+      // .refine((data) => {
+      //   const { raw_scores } = data.cognitive_load
+      //   const overall_score = Object.values(raw_scores).reduce((acc, score) => acc + score, 0)
+      //   const overall_category =
+      //     overall_score <= 33 ? 'Low' : overall_score <= 67 ? 'Medium' : 'High'
+      //   return (
+      //     data.cognitive_load.overall_score === overall_score &&
+      //     data.cognitive_load.overall_category === overall_category
+      //   )
+      // }),
+      discomfort: z.object({
+        raw_scores: overall_discomfort,
+        categories: z.object({
+          hand_wrist: z.enum(['Low', 'Medium', 'High']),
+          physical_demand: z.enum(['Low', 'Medium', 'High']),
+          chest: z.enum(['Low', 'Medium', 'High']),
+          upper_arm: z.enum(['Low', 'Medium', 'High']),
+          shoulder: z.enum(['Low', 'Medium', 'High']),
+          lower_back: z.enum(['Low', 'Medium', 'High']),
+          thigh: z.enum(['Low', 'Medium', 'High']),
+          neck: z.enum(['Low', 'Medium', 'High']),
+          lower_leg_foot: z.enum(['Low', 'Medium', 'High']),
+        }),
+      }),
+      exertion: z.object({ raw_scores: exertion, categories: z.enum(['Low', 'Medium', 'High']) }),
+      balance: z.object({ raw_scores: balance, categories: z.enum(['Low', 'Medium', 'High']) }),
     }),
   )
+
   //  "{input} \n\n Please analyze the following text and return the cognitive load, discomfort, exertion, and balance in JSON format. The JSON should contain the following keys: cognitive_load, discomfort, exertion, and balance. The values should be numbers between 0 and 20 for cognitive_load and balance, and between 0 and 10 for discomfort. If the number is not between the specified range, please return '0'.",
 
   const chain = RunnableSequence.from([
-    PromptTemplate.fromTemplate(
-      `
+    PromptTemplate.fromTemplate(`
+        Input:
+        {input}
+        
         You are a data-analysis assistant. 
         Classify each metric below based on the following rules:
         
@@ -60,118 +99,163 @@ export const analyze = async (text) => {
         ---
         
         Return ONLY a valid JSON object with the following top-level keys:
-        - "cognitive_load": {
-            "raw_scores": <input values>,
-            "categories": <Low/Medium/High per construct>,
-            "overall_score": <sum of constructs>,
-            "overall_category": <Low/Medium/High>
-          }
-        - "discomfort": {
-            "raw_scores": <input values>,
-            "categories": <Low/Medium/High per body part>
-          }
-        - "exertion": {
-            "raw_scores": <input value>,
-            "categories": <Low/Medium/High>
-          }
-        - "balance": {
-            "raw_scores": <input value>,
-            "categories": <Low/Medium/High>
-          }
+        - "cognitive_load": {{
+            "raw_scores": {{
+              "mental_demand": number,
+              "physical_demand": number,
+              "temporal_demand": number,
+              "performance": number,
+              "effort": number,
+              "frustration": number
+            }},
+            "categories": {{
+              "mental_demand": "Low" | "Medium" | "High",
+              "physical_demand": "Low" | "Medium" | "High",
+              "temporal_demand": "Low" | "Medium" | "High",
+              "performance": "Low" | "Medium" | "High",
+              "effort": "Low" | "Medium" | "High",
+              "frustration": "Low" | "Medium" | "High"
+            }},
+            "overall_score": number,
+            "overall_category": "Low" | "Medium" | "High"
+          }}
+        - "discomfort": {{
+            "raw_scores": {{
+              "hand_wrist": number,
+              "physical_demand": number,
+              "chest": number,
+              "upper_arm": number,
+              "shoulder": number,
+              "lower_back": number,
+              "thigh": number,
+              "neck": number,
+              "lower_leg_foot": number
+            }},
+            "categories": {{
+              "hand_wrist": "Low" | "Medium" | "High",
+              "physical_demand": "Low" | "Medium" | "High",
+              "chest": "Low" | "Medium" | "High",
+              "upper_arm": "Low" | "Medium" | "High",
+              "shoulder": "Low" | "Medium" | "High",
+              "lower_back": "Low" | "Medium" | "High",
+              "thigh": "Low" | "Medium" | "High",
+              "neck": "Low" | "Medium" | "High",
+              "lower_leg_foot": "Low" | "Medium" | "High"
+            }}
+          }}
+        - "exertion": {{
+            "raw_scores": number,
+            "categories": "Low" | "Medium" | "High"
+          }}
+        - "balance": {{
+            "raw_scores": number,
+            "categories": "Low" | "Medium" | "High"
+          }}
         
         DO NOT return any text or explanation outside this JSON.
-        
-        Input:
-        {input}
-      `,
-    ),
+        `),
     model,
     parser,
   ])
+
+  try {
+    const result = await chain.invoke({ input: inputData })
+    return result
+  } catch (error) {
+    console.error('Error:', error)
+    throw error
+  }
 }
 
-const boundedNumber = (min, max) => z.preprocess((val) => Number(val), z.number().min(min).max(max))
+const boundedNumber = (min, max, description) =>
+  z
+    .preprocess((val) => Number(val), z.number().min(min).max(max))
+    .describe(description || `Must be a number between ${min} and ${max}`)
 
-const overall_cognitive_load = z.object({
-  mental_demand: z
-    .string()
-    .describe(
-      "mental demand of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
-  physical_demand: z
-    .string()
-    .describe(
-      "physical demand of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
-  temporal_demand: z
-    .string()
-    .describe(
-      "temporal demand of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
-  performance: z
-    .string()
-    .describe(
-      "performance of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
-  effort: z
-    .string()
-    .describe(
-      "effort of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
-  frustration: z
-    .string()
-    .describe(
-      "frustration of the cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
-    ),
+export const overall_cognitive_load = z.object({
+  mental_demand: boundedNumber(
+    0,
+    20,
+    "Mental demand of cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
+  physical_demand: boundedNumber(
+    0,
+    20,
+    "Physical demand of cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
+  temporal_demand: boundedNumber(
+    0,
+    20,
+    "Temporal demand of cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
+  performance: boundedNumber(
+    0,
+    20,
+    "Performance metric of cognitive load, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
+  effort: boundedNumber(
+    0,
+    20,
+    "Effort involved in the task, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
+  frustration: boundedNumber(
+    0,
+    20,
+    "Level of frustration during the task, should be a number between 0 and 20. If the number is not between 0 and 20, please return '0'.",
+  ),
 })
 
-const overall_discomfort = z.object({
-  hand_wrist: z
-    .string()
-    .describe(
-      "hand and wrist discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  chest: z
-    .string()
-    .describe(
-      "chest discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  upper_arm: z
-    .string()
-    .describe(
-      "upper arm discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  shoulder: z
-    .string()
-    .describe(
-      "shoulder discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  lower_back: z
-    .string()
-    .describe(
-      "lower back discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  thigh: z
-    .string()
-    .describe(
-      "thigh discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  neck: z
-    .string()
-    .describe(
-      "neck discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
-  lower_leg_foot: z
-    .string()
-    .describe(
-      "lower leg and foot discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
-    ),
+export const overall_discomfort = z.object({
+  hand_wrist: boundedNumber(
+    0,
+    10,
+    "Hand and wrist discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  chest: boundedNumber(
+    0,
+    10,
+    "Chest discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  upper_arm: boundedNumber(
+    0,
+    10,
+    "Upper arm discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  shoulder: boundedNumber(
+    0,
+    10,
+    "Shoulder discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  lower_back: boundedNumber(
+    0,
+    10,
+    "Lower back discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  thigh: boundedNumber(
+    0,
+    10,
+    "Thigh discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  neck: boundedNumber(
+    0,
+    10,
+    "Neck discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
+  lower_leg_foot: boundedNumber(
+    0,
+    10,
+    "Lower leg and foot discomfort, should be a number between 0 and 10. If the number is not between 0 and 10, please return '0'.",
+  ),
 })
 
-const exertion = z.string.describe(
-  'exertion, should be a number between 0 and 20. If the number is not between 0 and 20, please return "0".',
+export const exertion = boundedNumber(
+  0,
+  20,
+  'Exertion, should be a number between 0 and 20. If the number is not between 0 and 20, please return "0".',
 )
 
-const balance = z.string.describe(
-  'balance, should be a number between 0 and 10. If the number is not between 0 and 10, please return "0".',
+export const balance = boundedNumber(
+  0,
+  10,
+  'Balance, should be a number between 0 and 10. If the number is not between 0 and 10, please return "0".',
 )
